d4e1427d1b1cc70246e9cee4188c8ebc01a81be5
Alfonso <alfonso.balmori@skydropx.com>
2024-06-13 14:08:30 -0600

    add code

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,19 +2,16 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Crear VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-# Crear Subnet
 resource "aws_subnet" "main" {
   count = 2
   cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
   vpc_id = aws_vpc.main.id
 }
 
-# Crear Security Group para Lambda
 resource "aws_security_group" "lambda_sg" {
   name_prefix = "lambda_sg_"
   vpc_id = aws_vpc.main.id
@@ -34,7 +31,11 @@ resource "aws_security_group" "lambda_sg" {
   }
 }
 
-# Crear Instancia RDS en la misma VPC y Subnet
+resource "aws_db_subnet_group" "main" {
+  name       = "main-subnet-group"
+  subnet_ids = aws_subnet.main[*].id
+}
+
 resource "aws_db_instance" "default" {
   allocated_storage      = 20
   engine                 = "mysql"
@@ -49,13 +50,13 @@ resource "aws_db_instance" "default" {
   db_subnet_group_name   = aws_db_subnet_group.main.name
 }
 
-resource "aws_db_subnet_group" "main" {
-  name       = "main"
-  subnet_ids = aws_subnet.main[*].id
+data "aws_iam_role" "existing_lambda_exec_role" {
+  name = "lambda_exec_role"
 }
 
-# Crear rol de ejecución para Lambda
 resource "aws_iam_role" "lambda_exec_role" {
+  count = length(data.aws_iam_role.existing_lambda_exec_role) == 0 ? 1 : 0
+
   name = "lambda_exec_role"
 
   assume_role_policy = jsonencode({
@@ -72,17 +73,17 @@ resource "aws_iam_role" "lambda_exec_role" {
   })
 }
 
-# Adjuntar políticas al rol de ejecución de Lambda
 resource "aws_iam_role_policy_attachment" "lambda_exec_policy" {
+  count = length(data.aws_iam_role.existing_lambda_exec_role) == 0 ? 1 : 0
+
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-# Crear función Lambda
 resource "aws_lambda_function" "docker_deploy_lambda" {
   filename         = "${path.module}/lambda_deploy.zip"
   function_name    = "docker_deploy_lambda"
-  role             = aws_iam_role.lambda_exec_role.arn
+  role             = coalesce(data.aws_iam_role.existing_lambda_exec_role.arn, aws_iam_role.lambda_exec_role.arn)
   handler          = "index.handler"
   runtime          = "nodejs18.x"
 
