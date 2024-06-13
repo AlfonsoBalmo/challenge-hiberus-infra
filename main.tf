1c61108a72f6026985cdf8142900d8f3abc9a00a
Alfonso <alfonso.balmori@skydropx.com>
2024-06-13 10:58:53 -0600

    add code

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "aws_db_instance" "mysql" {
   engine             = "mysql"
   engine_version     = "5.7"
   instance_class     = "db.t2.micro"
-  name               = var.db_name
+  db_name            = var.db_name
   username           = var.db_username
   password           = var.db_password
   db_subnet_group_name = aws_db_subnet_group.main.name
