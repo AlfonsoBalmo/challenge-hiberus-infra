32f1b7daad450ace5ba42685393af0e6c09fc5d7
Alfonso <alfonso.balmori@skydropx.com>
2024-06-13 11:07:57 -0600

    add code

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,6 +14,7 @@ resource "aws_subnet" "public" {
   count                   = 2
   vpc_id                  = aws_vpc.main.id
   cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
