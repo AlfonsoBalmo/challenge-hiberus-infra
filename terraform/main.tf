9cfa02e7dd3905ebe0f19f7568fa787d2ea26a0d
Alfonso <alfonso.balmori@skydropx.com>
2024-06-13 11:25:45 -0600

    add code

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ resource "aws_db_instance" "default" {
   engine               = "mysql"
   engine_version       = "8.0"
   instance_class       = "db.t2.micro"
-  name                 = var.db_name
+  db_name              = var.db_name
   username             = var.db_user
   password             = var.db_password
   parameter_group_name = "default.mysql8.0"
