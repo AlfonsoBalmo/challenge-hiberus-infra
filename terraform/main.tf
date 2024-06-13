27d9a96118b6d730df5481aada5b60639ca8c7eb
Alfonso <alfonso.balmori@skydropx.com>
2024-06-13 13:23:31 -0600

    add code

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,23 +32,46 @@ resource "aws_security_group" "ecs_sg" {
 }
 
 resource "aws_ecr_repository" "myapp" {
-  name = "hiberus"
+  name = "myapp-unique"
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 resource "aws_ecs_cluster" "main" {
-  name = "ecs-cluster-hiberus"
+  name = "ecs-cluster"
 }
 
 resource "aws_ecs_task_definition" "app" {
-  family                   = "hiberus"
+  family                   = "my-app"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
-      name      = "hiberus"
+      name      = "my-app"
       image     = "${aws_ecr_repository.myapp.repository_url}:latest"
       cpu       = 256
       memory    = 512
@@ -71,7 +94,7 @@ resource "aws_ecs_task_definition" "app" {
 }
 
 resource "aws_ecs_service" "app" {
-  name            = "my-app-service-hiberus"
+  name            = "my-app-service"
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.app.arn
   desired_count   = 1
