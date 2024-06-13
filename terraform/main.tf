d9bff957d4b61d2c2afc3b48b17a4a3cfbe0a003
Alfonso <alfonso.balmori@skydropx.com>
2024-06-13 14:41:30 -0600

    add code

--- terraform/main.tf
+++ terraform/main.tf
@@ -165,19 +165,3 @@ resource "aws_lb_listener" "http" {
 resource "aws_ecr_repository" "myapp" {
   name = "myapp"
 }
-
-output "ecs_service_name" {
-  value = aws_ecs_service.app.name
-}
-
-output "ecs_task_definition" {
-  value = aws_ecs_task_definition.app.family
-}
-
-output "ecr_repository_url" {
-  value = aws_ecr_repository.myapp.repository_url
-}
-
-output "alb_dns" {
-  value = aws_lb.app.dns_name
-}
